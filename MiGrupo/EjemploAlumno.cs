8b3081da507a3a20aac9d11c3858db78e28f11c0
Lucheti <luksdlt92@gmail.com>
2014-11-01 00:51:51 -0300

    Fix del commit anterior

--- MiGrupo/EjemploAlumno.cs
+++ MiGrupo/EjemploAlumno.cs
@@ -13,65 +13,6 @@ using TgcViewer.Utils.TgcSceneLoader;
 using Microsoft.DirectX.DirectInput;
 using AlumnoEjemplos.MiGrupo.Entities;
 using TgcViewer.Utils.TgcSkeletalAnimation;
-
-namespace AlumnoEjemplos.MiGrupo
-{
-    public class EjemploAlumno : TgcExample
-    {
-        TgcScene taxiScene;
-        TgcScene ciudadScene;
-
-        public override string getCategory()
-        {
-            return "AlumnoEjemplos";
-        }
-
-        /// <summary>
-        /// Completar nombre del grupo en formato Grupo NN
-        /// </summary>
-        public override string getName()
-        {
-            return "Grupo LOS_BARTO";
-        }
-
-        public override string getDescription()
-        {
-            return "CrazyTaxi - Un taxi que debe llevar pasajeros de un punto de la ciudad a otro en un tiempo establecido al menos 5 veces.";
-        }
-
-        public override void init()
-        {
-            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
-
-            //Carpeta de archivos Media del alumno
-
-            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
-            //primero cargamos una escena 3D entera.
-            TgcSceneLoader loader = new TgcSceneLoader();
-
-            //Luego cargamos otro modelo aparte que va a hacer el taxi
-            taxiScene = loader.loadSceneFromFile(alumnoMediaFolder + "LOS_BARTO\\taxi\\taxi-TgcScene.xml");
-            //Cargamos la ciudad
-            ciudadScene = loader.loadSceneFromFile(alumnoMediaFolder + "LOS_BARTO\\ciudad\\ciudad-TgcScene.xml");
-
-            //solo nos interesa el taxi
-            Auto.getInstance().inicializar(taxiScene.Meshes[0]);
-            Flecha.getInstance().inicializar();
-using System;
-using System.Collections.Generic;
-using System.Text;
-using TgcViewer.Example;
-using TgcViewer;
-using Microsoft.DirectX.Direct3D;
-using System.Drawing;
-using Microsoft.DirectX;
-using TgcViewer.Utils.Modifiers;
-using TgcViewer.Utils.TgcGeometry;
-using TgcViewer.Utils.Input;
-using TgcViewer.Utils.TgcSceneLoader;
-using Microsoft.DirectX.DirectInput;
-using AlumnoEjemplos.MiGrupo.Entities;
-using TgcViewer.Utils.TgcSkeletalAnimation;
 using TgcViewer.Utils.Shaders;
 using System.Windows.Forms;
 using TgcViewer.Utils.Terrain;
